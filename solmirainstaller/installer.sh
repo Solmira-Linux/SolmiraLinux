d06403d79f7027860869ffdd814cec6a2ef53b7a
Lazymanzoid <185846876+LazymanzoidOfficial@users.noreply.github.com>
2025-12-11 21:16:20 -0500

    Add progress gauge to installation steps
    Refactor installer script to include progress gauge steps.

--- solmirainstaller/installer.sh
+++ solmirainstaller/installer.sh
@@ -1,24 +1,44 @@
 #!/bin/bash
 set -e
 
-# Load functions
 source "$(dirname "$0")/functions.sh"
 
-# ---------- START ----------
 check_root
 welcome_screen
 
-TARGET_DISK=$(select_disk)
-confirm_wipe "$TARGET_DISK"
+# Start gauge
+(
+    gauge_step 5  "Selecting disk..."
+    TARGET_DISK=$(select_disk)
 
-create_partitions_fdisk "$TARGET_DISK"
-format_partitions
-mount_partitions
+    gauge_step 10 "Confirming wipe..."
+    confirm_wipe "$TARGET_DISK"
 
-set_hostname
-create_user
-install_base
-generate_fstab
-install_bootloader
+    gauge_step 20 "Partitioning disk..."
+    create_partitions_fdisk "$TARGET_DISK"
+
+    gauge_step 35 "Formatting partitions..."
+    format_partitions
+
+    gauge_step 45 "Mounting partitions..."
+    mount_partitions
+
+    gauge_step 55 "Setting hostname..."
+    set_hostname
+
+    gauge_step 60 "Creating user..."
+    create_user
+
+    gauge_step 75 "Installing base system (pacstrap)..."
+    install_base
+
+    gauge_step 85 "Generating fstab..."
+    generate_fstab
+
+    gauge_step 95 "Installing bootloader..."
+    install_bootloader
+
+    gauge_step 100 "Done!"
+) | whiptail --gauge "Installing Solmira Linux..." 12 60 0
 
 finish_screen
