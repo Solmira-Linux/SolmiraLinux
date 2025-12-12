275b277f305b00620f86525cab504b6c604d18a7
Lazymanzoid <185846876+LazymanzoidOfficial@users.noreply.github.com>
2025-12-11 21:14:46 -0500

    Refactor Solmira installer script
    Refactor installer script to streamline the installation process and remove unnecessary functions.

--- solmirainstaller/installer.sh
+++ solmirainstaller/installer.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
-# Solmira Linux Installer (Whiptail)
+set -e
 
-# Load helper functions
+# Load functions
 source "$(dirname "$0")/functions.sh"
 
-function welcome() {
-    whiptail --title "Hello! :D" --msgbox "Welcome to Solmira Linux! This setup will guide you towards installing Solmira onto your computer." 10 60
-}
+# ---------- START ----------
+check_root
+welcome_screen
 
-function select_disk() {
-    local DISK
-    DISK=$(lsblk -d -n -o NAME,SIZE | awk '{print $1 " " $2}' | \
-        whiptail --title "Select Disk" --menu "Choose target disk:" 20 60 10 $(cat) 3>&1 1>&2 2>&3)
-    echo "$DISK"
-}
-
-function confirm_disk() {
-    local DISK="$1"
-    whiptail --yesno "Are you sure you want to install on /dev/$DISK?" 10 60
-}
-
-# Main flow
-welcome
 TARGET_DISK=$(select_disk)
-confirm_disk "$TARGET_DISK"
-# Next: partitioning, filesystem, packages, bootloader, users
+confirm_wipe "$TARGET_DISK"
+
+create_partitions_fdisk "$TARGET_DISK"
+format_partitions
+mount_partitions
+
+set_hostname
+create_user
+install_base
+generate_fstab
+install_bootloader
+
+finish_screen
