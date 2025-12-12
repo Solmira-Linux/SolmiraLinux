421d260decb7be512c26034730386ddcf60d3744
Lazymanzoid <185846876+LazymanzoidOfficial@users.noreply.github.com>
2025-12-11 21:29:24 -0500

    Reorder gauge steps in installer script

--- solmirainstaller/installer.sh
+++ solmirainstaller/installer.sh
@@ -6,7 +6,6 @@ source "$(dirname "$0")/functions.sh"
 check_root
 welcome_screen
 
-# Start gauge
 (
     gauge_step 5  "Selecting disk..."
     TARGET_DISK=$(select_disk)
@@ -17,28 +16,36 @@ welcome_screen
     gauge_step 20 "Partitioning disk..."
     create_partitions_fdisk "$TARGET_DISK"
 
-    gauge_step 35 "Formatting partitions..."
+    gauge_step 30 "Formatting partitions..."
     format_partitions
 
-    gauge_step 45 "Mounting partitions..."
+    gauge_step 40 "Mounting partitions..."
     mount_partitions
 
-    gauge_step 55 "Setting hostname..."
-    set_hostname
-
-    gauge_step 60 "Creating user..."
-    create_user
-
-    gauge_step 75 "Installing base system (pacstrap)..."
+    gauge_step 55 "Installing base system..."
     install_base
 
-    gauge_step 85 "Generating fstab..."
+    gauge_step 65 "Generating fstab..."
     generate_fstab
 
-    gauge_step 95 "Installing bootloader..."
+    gauge_step 70 "Setting hostname..."
+    set_hostname
+
+    gauge_step 75 "Configuring locale..."
+    set_locale
+
+    gauge_step 80 "Setting timezone..."
+    set_timezone
+
+    gauge_step 85 "Setting keymap..."
+    set_keymap
+
+    gauge_step 90 "Creating user..."
+    create_user
+
+    gauge_step 100 "Installing bootloader..."
     install_bootloader
 
-    gauge_step 100 "Done!"
 ) | whiptail --gauge "Installing Solmira Linux..." 12 60 0
 
 finish_screen
