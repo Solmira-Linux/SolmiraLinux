07b7c02dda473790b239170c67251e84595c31a7
LazymanzoidOfficial <lazymanzoidofficial@noreply.codeberg.org>
2025-12-13 17:02:38 -0500

    Fixed installer (sorta) and added solmira-packages repo

--- solmira/airootfs/usr/local/bin/functions.sh
+++ solmira/airootfs/usr/local/bin/functions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 LOG="/tmp/solmira-install.log"
 
@@ -21,15 +22,14 @@ gauge_step() {
 # -----------------------------
 check_root() {
     if [ "$(id -u)" != 0 ]; then
-        whiptail --title "Error" \
-        --msgbox "This installer must be launched as root." 8 50
+        whiptail --title "Error" --msgbox "This installer must be run as root." 8 50
         exit 1
     fi
 }
 
 welcome_screen() {
-    whiptail --title "Hello! :D" \
-    --msgbox "Welcome to Solmira Linux!\nThis installer will guide you through installing Solmira Linux on your computer." 10 60
+    whiptail --title "Welcome to Solmira Linux!" \
+        --msgbox "This installer will guide you through installing Solmira Linux on your computer." 10 60
 }
 
 # -----------------------------
@@ -37,21 +37,19 @@ welcome_screen() {
 # -----------------------------
 select_disk() {
     mapfile -t items < <(lsblk -d -o NAME,SIZE | tail -n +2 | awk '{print "/dev/"$1, $2}')
+    [ "${#items[@]}" -eq 0 ] && { whiptail --msgbox "No disks detected." 8 40; exit 1; }
 
-    CHOICE=$(whiptail --title "Select Disk" \
+    whiptail --title "Select Disk" \
         --menu "Choose installation disk:" 20 60 10 \
-        "${items[@]}" 3>&1 1>&2 2>&3)
-
-    echo "$CHOICE"
+        "${items[@]}" 3>&1 1>&2 2>&3
 }
 
 confirm_wipe() {
-    DEV="$1"
-    whiptail --yesno "WARNING: This will ERASE ALL DATA on $DEV. Continue?" 10 60 || exit 1
+    whiptail --yesno "WARNING: This will ERASE ALL DATA on $1.\nContinue?" 10 60 || exit 1
 }
 
 # -----------------------------
-# PARTITIONING (fdisk)
+# PARTITIONING
 # -----------------------------
 create_partitions_fdisk() {
     DEV="$1"
@@ -64,79 +62,67 @@ n
 +1G
 t
 1
+1
 n
 2
 
 
 t
 2
-23
+20
 w
 EOF
 
     base=$(basename "$DEV")
-
     if [[ "$base" =~ ^(nvme|mmcblk) ]]; then
-        export EFI="/dev/${base}p1"
-        export ROOT="/dev/${base}p2"
+        echo "/dev/${base}p1 /dev/${base}p2"
     else
-        export EFI="/dev/${base}1"
-        export ROOT="/dev/${base}2"
+        echo "/dev/${base}1 /dev/${base}2"
     fi
 }
 
 format_partitions() {
+    EFI="$1"
+    ROOT="$2"
     mkfs.fat -F32 "$EFI"
     mkfs.ext4 -F "$ROOT"
 }
 
 mount_partitions() {
+    EFI="$1"
+    ROOT="$2"
+
     mount "$ROOT" /mnt
     mkdir -p /mnt/boot/efi
     mount "$EFI" /mnt/boot/efi
 }
 
 # -----------------------------
-# SYSTEM CONFIG
+# SYSTEM CONFIGURATION
 # -----------------------------
 set_locale() {
-    LOCALE=$(whiptail --inputbox "Enter locale (default: en_US.UTF-8):" \
-        10 60 "en_US.UTF-8" 3>&1 1>&2 2>&3)
-
-    arch-chroot /mnt sed -i "s/#$LOCALE UTF-8/$LOCALE UTF-8/" /etc/locale.gen
+    arch-chroot /mnt sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
     arch-chroot /mnt locale-gen
-
-    echo "LANG=$LOCALE" > /mnt/etc/locale.conf
+    echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 }
 
 set_timezone() {
-    TIMEZONE=$(whiptail --inputbox "Enter timezone (example: America/New_York):" \
-        10 60 "America/New_York" 3>&1 1>&2 2>&3)
-
-    arch-chroot /mnt ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
+    arch-chroot /mnt ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
     arch-chroot /mnt hwclock --systohc
 }
 
 set_keymap() {
-    KEYMAP=$(whiptail --inputbox "Enter keyboard layout (default: us):" \
-        10 60 "us" 3>&1 1>&2 2>&3)
-
-    echo "KEYMAP=$KEYMAP" > /mnt/etc/vconsole.conf
+    echo "KEYMAP=us" > /mnt/etc/vconsole.conf
 }
 
 set_hostname() {
-    HOST=$(whiptail --inputbox "Enter hostname:" \
-        10 60 "solmira" 3>&1 1>&2 2>&3)
-
+    HOST="$1"
     echo "$HOST" > /mnt/etc/hostname
 }
 
 create_user() {
-    USERNAME=$(whiptail --inputbox "Enter username:" \
-        10 60 "user" 3>&1 1>&2 2>&3)
-
-    PASS=$(whiptail --passwordbox "Enter password:" \
-        10 60 3>&1 1>&2 2>&3)
+    USERNAME="$1"
+    PASS="$2"
 
     echo "root:$PASS" | arch-chroot /mnt chpasswd
     arch-chroot /mnt useradd -m -G wheel "$USERNAME"
@@ -144,34 +130,26 @@ create_user() {
 
     arch-chroot /mnt bash -c 'echo "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/00-wheel'
     arch-chroot /mnt chmod 440 /etc/sudoers.d/00-wheel
-
-    
 }
 
-
+# -----------------------------
+# AUR HELPER
+# -----------------------------
 ask_aur() {
-    if whiptail --yesno "Would you like to enable the Arch User Repository (AUR) using paru?" 10 60 ; then
-        echo "yes"
-    else
-        echo "no"
-    fi
+    whiptail --yesno "Enable AUR using paru?" 10 60 && echo yes || echo no
 }
-
 
 install_paru() {
-    USERNAME="$1"
+    USER="$1"
 
-    # Install dependencies for makepkg
     arch-chroot /mnt pacman -S --noconfirm --needed base-devel git
 
-    # Build paru in user's home directory
-    arch-chroot /mnt bash -c "
-        sudo -u $USERNAME bash -c '
-            cd ~ &&
-            git clone https://aur.archlinux.org/paru.git &&
-            cd paru &&
-            makepkg -si --noconfirm
-        '
+    arch-chroot /mnt sudo -u "$USER" bash -c "
+        rustup default stable &&
+        cd ~ &&
+        git clone https://aur.archlinux.org/paru.git &&
+        cd paru &&
+        makepkg -si --noconfirm
     "
 }
 
@@ -179,11 +157,7 @@ install_paru() {
 # BASE SYSTEM
 # -----------------------------
 install_base() {
-    # Base system packages
-    pacstrap /mnt base linux linux-firmware linux-headers networkmanager grub efibootmgr base-devel sudo nano vim
-
-    # KDE Plasma and other applications
-    pacstrap /mnt plasma-meta dolphin konsole kate partitionmanager okular libreoffice-still firefox gwenview kalk haruna elisa rustup gamemode gamescope gimp inkscape rustup flatpak && rustup default stable
+    pacstrap /mnt solmira-desktop
 }
 
 generate_fstab() {
@@ -191,11 +165,14 @@ generate_fstab() {
 }
 
 install_bootloader() {
-    arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Solmira Linux"
+    arch-chroot /mnt grub-install \
+        --target=x86_64-efi \
+        --efi-directory=/boot/efi \
+        --bootloader-id="Solmira Linux"
     arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 }
 
 finish_screen() {
     whiptail --title "Done! :D" \
-    --msgbox "Solmira Linux has been successfully installed.\nYou may now reboot." 10 60
+        --msgbox "Solmira Linux has been successfully installed.\nYou may now reboot." 10 60
 }
--- solmira/airootfs/usr/local/bin/installer.sh
+++ solmira/airootfs/usr/local/bin/installer.sh
@@ -3,6 +3,8 @@ set -e
 
 source "$(dirname "$0")/functions.sh"
 
+LOG="/tmp/solmira-install.log"
+
 # -----------------------------
 # PRE-GAUGE INTERACTIVE PROMPTS
 # -----------------------------
@@ -10,54 +12,55 @@ source "$(dirname "$0")/functions.sh"
 check_root
 welcome_screen
 
-# Disk selection and wipe confirmation
+# Disk selection
 TARGET_DISK=$(select_disk)
+[ -z "$TARGET_DISK" ] && exit 1
 confirm_wipe "$TARGET_DISK"
 
-# System configuration prompts
-set_hostname
-set_locale
-set_timezone
-set_keymap
+# User input
+HOSTNAME=$(whiptail --inputbox "Hostname:" 10 60 "solmira" 3>&1 1>&2 2>&3)
+USERNAME=$(whiptail --inputbox "Username:" 10 60 "user" 3>&1 1>&2 2>&3)
+PASSWORD=$(whiptail --passwordbox "Password:" 10 60 3>&1 1>&2 2>&3)
 
-# User creation
-create_user
-export USERNAME  # make available for AUR installation
-
-# Ask about AUR
 AUR_CHOICE=$(ask_aur)
+
+# Partition BEFORE gauge
+read EFI ROOT <<< "$(create_partitions_fdisk "$TARGET_DISK")"
 
 # -----------------------------
 # GAUGE FOR LONG-RUNNING STEPS
 # -----------------------------
 (
-    gauge_step 20 "Partitioning disk..."
-    create_partitions_fdisk "$TARGET_DISK"
+    gauge_step 20 "Formatting partitions..."
+    format_partitions "$EFI" "$ROOT" >>"$LOG" 2>&1
 
-    gauge_step 30 "Formatting partitions..."
-    format_partitions
+    gauge_step 30 "Mounting partitions..."
+    mount_partitions "$EFI" "$ROOT" >>"$LOG" 2>&1
 
-    gauge_step 40 "Mounting partitions..."
-    mount_partitions
-
-    gauge_step 55 "Installing base system..."
-    install_base
+    gauge_step 50 "Installing base system..."
+    pacstrap /mnt solmira-desktop >>"$LOG" 2>&1
 
     gauge_step 65 "Generating fstab..."
-    generate_fstab
+    generate_fstab >>"$LOG" 2>&1
+
+    gauge_step 75 "Configuring system..."
+    set_locale >>"$LOG" 2>&1
+    set_timezone >>"$LOG" 2>&1
+    set_keymap >>"$LOG" 2>&1
+    set_hostname "$HOSTNAME" >>"$LOG" 2>&1
+    create_user "$USERNAME" "$PASSWORD" >>"$LOG" 2>&1
 
     if [ "$AUR_CHOICE" = "yes" ]; then
-        gauge_step 75 "Installing AUR helper..."
-        install_paru "$USERNAME"
+        gauge_step 85 "Installing AUR helper..."
+        install_paru "$USERNAME" >>"$LOG" 2>&1
     fi
 
-    gauge_step 90 "Installing bootloader..."
-    install_bootloader
+    gauge_step 95 "Installing bootloader..."
+    install_bootloader >>"$LOG" 2>&1
 
-    gauge_step 100 "Done!"
+    gauge_step 100 "Finishing..."
 ) | whiptail --gauge "Installing Solmira Linux..." 12 60 0
 
-# -----------------------------
-# FINISH
-# -----------------------------
 finish_screen
+
+echo "Installation complete! You can check the full log at $LOG"
