491c4ebf36a3591b98f45e9a0a13d64734c2bce1
LazymanzoidOfficial <lazymanzoidofficial@noreply.codeberg.org>
2025-12-14 18:32:55 +0100

    Changed installer to use infoboxes instead of gauges

--- solmira/airootfs/usr/local/bin/installer.sh
+++ solmira/airootfs/usr/local/bin/installer.sh
@@ -6,14 +6,6 @@ exec > >(tee -a "$LOG") 2>&1
 
 source "$(dirname "$0")/functions.sh"
 
-
-gauge_step() {
-    local percent="$1"
-    local msg="$2"
-    whiptail --gauge "$msg" 12 60 "$percent"
-    sleep 0.3
-}
-
 check_root
 welcome_screen
 
@@ -21,42 +13,43 @@ welcome_screen
 DISK=$(select_disk) || exit 0
 confirm_wipe "$DISK"
 
+
 HOSTNAME=$(whiptail --inputbox "Hostname:" 10 60 "solmira" 3>&1 1>&2 2>&3) || exit 0
 USERNAME=$(whiptail --inputbox "Username:" 10 60 "user" 3>&1 1>&2 2>&3) || exit 0
 PASSWORD=$(whiptail --passwordbox "Password:" 10 60 3>&1 1>&2 2>&3) || exit 0
 
+
+whiptail --title "Partitioning: 10%" --infobox "Creating partitions on $DISK..." 8 60
 read EFI ROOT <<< "$(create_partitions "$DISK")" || fail "Partitioning failed."
 
 
-gauge_step 10 "Formatting partitions..."
+whiptail --title "Formatting: 25%" --infobox "Formatting EFI and root partitions..." 8 60
 format_partitions "$EFI" "$ROOT" || fail "Formatting failed."
 
-gauge_step 25 "Mounting filesystem..."
+
+whiptail --title "Mounting: 40%" --infobox "Mounting filesystem..." 8 60
 mount_partitions "$EFI" "$ROOT" || fail "Mount failed."
 
-gauge_step 40 "Installing base system..."
-install_base || fail "pacstrap failed."
 
-gauge_step 55 "Generating fstab..."
-generate_fstab || fail "fstab failed."
+whiptail --title "Installing Base System: 55%" --infobox "Installing Solmira Linux base system..." 8 60
+install_base || fail "Base system installation failed."
 
-gauge_step 70 "Configuring system..."
+
+whiptail --title "Generating fstab: 70%" --infobox "Generating fstab..." 8 60
+generate_fstab || fail "Fstab generation failed."
+
+
+whiptail --title "Configuring System: 85%" --infobox "Configuring locale, timezone, keymap, hostname, and user..." 8 60
 set_locale || fail "Locale setup failed."
 set_timezone || fail "Timezone setup failed."
 set_keymap || fail "Keymap setup failed."
 set_hostname "$HOSTNAME" || fail "Hostname setup failed."
 create_user "$USERNAME" "$PASSWORD" || fail "User creation failed."
 
-#AUR_CHOICE=$(ask_aur)
-#if [ "$AUR_CHOICE" = "yes" ]; then
- #   gauge_step 85 "Installing AUR helper..."
-  #  install_paru "$USERNAME" || fail "AUR helper installation failed."
-#fi
 
-gauge_step 95 "Installing bootloader..."
+whiptail --title "Installing Bootloader: 95%" --infobox "Installing GRUB bootloader..." 8 60
 install_bootloader || fail "Bootloader installation failed."
 
-gauge_step 100 "Finishing installation..."
-finish_screen
 
-echo "Installation has finished! You can read the full log at $LOG"
+whiptail --title "Installation Complete: 100%" --msgbox \
+"Solmira Linux was installed successfully!\nYou may now reboot.\nFull log: $LOG" 10 60
